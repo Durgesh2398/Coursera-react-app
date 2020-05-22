e4f10febe5256670a81360a688728fc1b0ba5310
Durgesh2398 <2308durgesh@gmail.com>
2020-05-22 22:05:53 +0530

    single page part 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Card,CardImg,CardImgOverlay,CardBody,CardTitle,CardText } from 'reactstrap';
-//import Dishdetail from './DishDetailComponent'
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 function RednerMenuItem({dish, onClick}) {
     return(
-           <Card onClick={() => onClick(dish.id)} >
+           <Card  >
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <Link to={`/menu/${dish.id}`} >
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
                     <CardImgOverlay>
                         <CardTitle>{dish.name}</CardTitle>
                    </CardImgOverlay>
+                   </Link>
                 </Card>
             );
     }
@@ -17,13 +21,23 @@ const Menu =(props)=>{
     const menu = props.dishes.map((dish) =>{
         return(
             <div key={dish.id} className="col-12 col-md-5 m-1">
-                <RednerMenuItem dish={dish} onClick={props.onClick} />
+                <RednerMenuItem dish={dish}  />
             </div>
             )
         });
 
         return (
             <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>
+                </div>
                 <div className="row">
                     {menu}
                 </div>
